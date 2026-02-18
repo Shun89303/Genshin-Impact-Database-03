d5d41e0902036de80790472ce1e5fb294c24d20d
Shun <ruka4662@gmail.com>
2026-02-18 18:51:12 +0630

    characters folder created in services for characters api, characters api setup complete, created hooks folder and useCharacters.ts file to fetch characters api and created zustand store useCharacterStore to store fetched ids, fixed global api unfinished code (#4)
    ok

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,4 +3,5 @@ import { BASE_URL } from "../config/env";
 export default async function apiClient(path: string) {
 	const res = await fetch(`${BASE_URL}${path}`);
 	if (!res.ok) return new Error("api fetch request failed");
+	return res.json();
 }
